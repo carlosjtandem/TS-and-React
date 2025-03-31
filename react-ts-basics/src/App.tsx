f9bf215920bae6eb7382c684c67cd0af1fefd26c
Carlos Aucancela <carlos.aucancela@infotrack.com>
2025-03-31 15:16:33 -0500

    Using type in a generic form. I mean we can use the type in al the pages.

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import { useState } from "react";
 import CourseGoalList from "./components/CourseGoalList";
 
-type CourseGoal = {
+export type CourseGoal = {
   title: string;
   description: string;
   id: number;
--- react-ts-basics/src/components/CourseGoalList.tsx
+++ react-ts-basics/src/components/CourseGoalList.tsx
@@ -1,14 +1,11 @@
 import CourseGoal from "./CourseGoal";
-
+import { type CourseGoal as CourseGoalType } from "../App"; // Importing the CourseGoal type from App.tsx. We need to change the name because we are using the same name.
 
 type CourseGoalListProps = {
-  goals: {
-    title: string;
-    description: string;
-    id: number;
-  }[];
+  goals: CourseGoalType[]; // The goals prop is an array of CourseGoal objects.	
 };
 
+//We have exported the CourseGoalListProps type so we can use it in this file an other as well.
 export default function CourseGoalList({ goals }: CourseGoalListProps) {
   return (
     <ul>
