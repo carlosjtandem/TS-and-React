63d22279ec738e12c1ba4ab0a34b9b83bfec3999
Carlos Aucancela <carlos.aucancela@infotrack.com>
2025-03-31 15:08:51 -0500

    First component - ListGoals

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -1,13 +1,40 @@
 import "./App.css";
-import CourseGoal from "./components/CourseGoal";
+import goalsImg from "./assets/react.svg";
+import Header from "./components/Header";
+import { useState } from "react";
+import CourseGoalList from "./components/CourseGoalList";
+
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
 
 function App() {
+  // const [goals, setGoals] = useState([]);  // common use, in this case the goals is an array of objects
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal() {
+    console.log("Add goal");
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        title: "New goal",
+        description: "New goal description",
+        id: Math.random(),
+      };
+      // const newGoals = [...prevGoals, newGoal];  // this is the same as the next line
+      return [...prevGoals, newGoal];
+    });
+  }
+
   return (
-    <>
-      <CourseGoal title="Lear React & TS">
-        <p>Learn from the ground up</p>
-      </CourseGoal>
-    </>
+    <div style={{ backgroundColor: "gray" }}>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
+        <h1>Course Goals</h1>
+      </Header>
+      <button onClick={handleAddGoal}>Add goal</button>
+      <CourseGoalList goals={goals} />
+    </div>
   );
 }
 
--- react-ts-basics/src/components/CourseGoal.tsx
+++ react-ts-basics/src/components/CourseGoal.tsx
@@ -1,20 +1,40 @@
-import { type PropsWithChildren } from "react";
+import { FC, type PropsWithChildren } from "react";
 
+//first appoach
 // type CourseGoalProps = {
 //   tittle: string;
-//   children: ReactNode;
+//   children: ReactNode;  //Every component in react receives a children prop. ReactNode is a type that can be any JSX code
+//   children: ReactNode[];  //If we want to receive multiple children
 // };
 
-type CourseGoalProps = PropsWithChildren<{ title: string }>;
+//second approach
+// type CourseGoalProps = PropsWithChildren<{ title: string }>;
 
-export default function CourseGoal({ title, children }: CourseGoalProps) {
+// export default function CourseGoal({ title, children }: CourseGoalProps) {
+//   //children is a special prop in react. In this case gets the <p> tag sent from App.tsx
+//   return (
+//     <article style={{ backgroundColor: "gray" }}>
+//       <div>
+//         <h2>{title}</h2>
+//         <h3>{children}</h3>
+//       </div>
+//       <button>DELETE</button>
+//     </article>
+//   );
+// }
+
+//Below another way to create the component
+type CourseGoalPropsv2 = PropsWithChildren<{ title: string }>;
+
+const CourseGoal_v2: FC<CourseGoalPropsv2> = ({ title, children }) => {
   return (
     <article>
       <div>
         <h2>{title}</h2>
-        <h2>{children}</h2>
+        <h3>{children}</h3>
       </div>
       <button>DELETE</button>
     </article>
   );
-}
+};
+export default CourseGoal_v2; //This is the way to export the component in the same line as the function declaration
