18b0ce763b3b57865125477b028986fc2cd4948c
Carlos Aucancela <carlos.aucancela@infotrack.com>
2025-04-01 15:28:35 -0500

    Added basic with Bootstrap and dark mode

---
+++ food-delivey-form/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="mx-5">
+        <div>tset sfds</div>;
+      </div>
+    </div>
+  );
+}
+
+export default App;
